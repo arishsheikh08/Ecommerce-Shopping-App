704a0579fd2efbef032be7a7bdb21c21c09194ce
DESKTOP-31NL1C6\Arish <arishsheikh08@gmail.com>
2022-07-11 17:44:52 +0530

    added sample image as banner

---
+++ ShopApp/src/modules/home/component/Banner.js
@@ -0,0 +1,40 @@
+// import * as React from 'react';
+// import {useEffect} from 'react';
+// import {Image, View} from 'react-native';
+
+// import Carousel from 'react-native-snap-carousel';
+
+
+
+// function Banners() {
+ 
+//   const banners = [
+//     require('../../../components/database/images/products/Adidas.jpg'),
+//   ]
+
+ 
+
+//   const _renderItem = ({item, index}) => {
+//     return (
+//       <View style={{flex: 1, minHeight: 150}}>
+//         <Image
+//           source={  require('../../../components/database/images/products/Adidas.jpg')}
+//           style={{width: 400, height: 220}}
+//           alt="Banner"
+//         />
+//       </View>
+//     );
+//   };
+
+//   return (
+//     <Carousel
+//       data={banners}
+//       renderItem={_renderItem}
+//       sliderWidth={400}
+//       itemWidth={400}
+//       autoplay={true}
+//     />
+//   );
+// }
+
+// export default Banners;
--- ShopApp/src/modules/home/screens/Home.js
+++ ShopApp/src/modules/home/screens/Home.js
@@ -1,12 +1,115 @@
 import React from 'react';
-import {View, Text } from 'react-native';
+import {View, Text,StyleSheet,Image } from 'react-native';
+import Banners from '../component/Banner';
 
 const HomeScreen = () => {
     return(
         <View>
             <Text>Home Screen</Text>
+            <View style={{ flex: 1 }}>
+                  <Image
+                    style={styles.SectionImg2}
+                    source={require('../../../components/database/images/products/Adidas.jpg')}
+                    alt="description of image"
+                  />
+                </View>
+            {/* <Banners /> */}
         </View>
     )
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    image1: {
+      height: 180,
+      width: 380,
+      marginLeft: 10,
+      alignItems: 'baseline',
+    },
+  
+    buttonStyle: {
+      backgroundColor: 'black',
+      color: '#FFFFFF',
+      borderColor: 'red',
+      height: 30,
+      width: 100,
+      alignItems: 'center',
+      paddingHorizontal: 10,
+      padding: 5,
+      marginLeft: 20,
+      flexDirection: 'row',
+    },
+    SectionImg2: {
+      borderColor: 'red',
+      height: 140,
+      width: 360,
+      alignItems: 'stretch',
+      paddingHorizontal: 10,
+      marginLeft: 20,
+      padding: 10,
+      paddingTop: 20,
+      marginBottom: 20,
+      marginTop: 10,
+    },
+    SectionImg3: {
+      borderColor: 'red',
+      height: 123,
+      width: 360,
+      alignItems: 'stretch',
+      paddingHorizontal: 10,
+      marginLeft: 20,
+      padding: 10,
+      marginBottom: 20,
+      marginTop: 10,
+    },
+    SectionImg4: {
+      borderColor: 'red',
+      height: 140,
+      width: 360,
+      alignItems: 'stretch',
+      paddingHorizontal: 10,
+      marginLeft: 20,
+      padding: 10,
+      marginBottom: 20,
+      marginTop: 10,
+    },
+    SectionImg5: {
+      borderColor: 'red',
+      height: 100,
+      width: 360,
+      alignItems: 'stretch',
+      paddingHorizontal: 10,
+      marginLeft: 20,
+      padding: 10,
+      marginTop: 20,
+    },
+  
+    buttonTextStyle: {
+      color: '#FFFFFF',
+      fontSize: 16,
+    },
+    inputText: {
+      fontSize: 22,
+      paddingHorizontal: 10,
+      marginLeft: 10,
+    },
+    inputText2: {
+      color: '#000000',
+      fontSize: 22,
+  
+      paddingHorizontal: 10,
+      marginBottom: 40,
+      marginLeft: 10,
+    },
+    btnViewAll: {
+      color: 'black',
+      fontSize: 14,
+      paddingTop: 5,
+    },
+  });
